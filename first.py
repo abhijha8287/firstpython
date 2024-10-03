a7d1fedf565d9ce25411a2cc251580681c67f8a4
Abhishek Jha <abhijha8287@gmail.com>
2024-10-03 17:40:13 +0530

    Try to push first Python File

---
+++ first.py
@@ -0,0 +1,12 @@
+# This is my first python file for the github push 
+# Although I learnt a lot of python in last one year
+
+def sum(l):
+    s=0
+    for i in l:
+        s+=i
+    return (s)
+
+# This is just the simple function in which we can add numbers.
+
+
